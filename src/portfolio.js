d594e1602a31057746329530b709700d07372997
Hafidz25 <hafidzalif80@gmail.com>
2022-09-30 11:17:14 +0700

    modified:   build/asset-manifest.json 	modified:   build/index.html 	new file:   build/static/js/main.0eac00df.chunk.js 	new file:   build/static/js/main.0eac00df.chunk.js.map 	deleted:    build/static/js/main.da474c4b.chunk.js 	deleted:    build/static/js/main.da474c4b.chunk.js.map 	new file:   build/static/js/runtime-main.b7afc8aa.js 	new file:   build/static/js/runtime-main.b7afc8aa.js.map 	deleted:    build/static/js/runtime-main.c4a74007.js 	deleted:    build/static/js/runtime-main.c4a74007.js.map 	modified:   package.json 	modified:   src/portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@
 
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://hafidz25.github.io/hafidz-portfolio-v2',
+  homepage: '',
   title: 'JS.',
 }
 
